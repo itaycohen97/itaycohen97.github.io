5aa9826af45e8b463e5924b7d927533cd1b94361
itaycohen97 <itay.cohen97@gmail.com>
2022-03-01 18:37:49 +0200

    added navbar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 function decide_width() {
   if (425 > window.screen.width) return "100%";
-  else return "300px";
+  else return "380px";
 }
 function toggleNav() {
-  if (document.getElementById("dropdownMenuContent").clientWidth == "0")
-    document.getElementById("dropdownMenuContent").style.width = decide_width();
-  else document.getElementById("dropdownMenuContent").style.width = "0";
+  if (document.getElementById("Sidenav").clientWidth == "0")
+    document.getElementById("Sidenav").style.width = decide_width();
+  else document.getElementById("Sidenav").style.width = "0";
 }
-
-const dropdownbtn = document.getElementById("dropdownMenuButton");
-
-function togglemenu() {
-  const menu = document.getElementById("dropdownMenuContent");
-  menu.classList.toggle("show");
-}
-
-dropdownbtn.addEventListener("click", togglemenu);
